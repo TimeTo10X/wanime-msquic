9cdf00b3d39078f943d151926a011229b20784a1
アクゼスティア <akzestia@gmail.com>
2026-01-31 17:54:20 +0900

    Update src/platform/platform_winkernel.c

--- src/platform/platform_winkernel.c
+++ src/platform/platform_winkernel.c
@@ -14,9 +14,7 @@ Environment:
 --*/
 
 #include "platform_internal.h"
-#ifdef QUIC_CLOG
-#include "platform_winkernel.c.clog.h"
-#endif
+
 
 typedef enum _SYSTEM_INFORMATION_CLASS {
     SystemBasicInformation                          = 0
